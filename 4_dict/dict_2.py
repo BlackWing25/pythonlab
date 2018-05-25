b57eec670515b90d91cd80456e0c636948a6ada1
BlackWing25 <sean860525@gmail.com>
2018-05-25 17:30:42 +0800

    dict test

---
+++ 4_dict/dict_2.py
@@ -0,0 +1,25 @@
+students = {}
+
+def test():
+    num = int(input())
+    # print(num)
+    for i in range(0, num):
+        line = input()
+        # print(line)
+        tokens = line.split()
+        # print(tokens)
+        # students["NCTU-1"] = 2000
+        students[tokens[0]+"-1"] = int(tokens[1])
+        students[tokens[0]+"-2"] = int(tokens[2])
+        students[tokens[0]+"-3"] = int(tokens[3])
+        students[tokens[0]+"-4"] = int(tokens[4])
+    # print(students)
+    num = int(input())
+    # print(num)
+    for i in range(0, num):
+        line = input()
+        # print(line, students[line])
+        print(line, students.get(line))
+    # print("Stanford-1", students["Stanford-1"])
+    print("Stanford-1", students.get("Stanford-1"))
+test()
