5a42e9ddf453bf7382d4cc616b0c0565b2550140
BlackWing25 <sean860525@gmail.com>
2018-05-25 17:28:48 +0800

    set

---
+++ 5_set/zoo.py
@@ -0,0 +1,17 @@
+students = [2000, 1900, 1800, 1700]
+
+age = {
+    "Alice": 15,
+    "Jimmy": 17,
+    "Griffin": 35
+}
+
+zooA = {'elephant', 'tiger', 'lion', 'giraffe', 'leopard', 'antelop'}
+zooB = {'lion', 'cheetah', 'giraffe', 'zebra', 'wildebeest', 'elephant'}
+
+print("zoo A", zooA)
+print("zoo B", zooB)
+print("zoo A and zoo B:", zooA & zooB)
+print("zoo A or zoo B:", zooA | zooB)
+print("zoo A - zoo B:", zooA - zooB)
+print("zoo A xor zoo B:", zooA ^ zooB)
